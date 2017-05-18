64f76eb9047559b74818f533471840f7aba4621a
Sterndata <subscribed-lists@sterndata.com>
2017-05-18 17:08:23 -0500

    put content in wp_options

--- modal-message.php
+++ modal-message.php
@@ -1,20 +1,29 @@
 <?php
-// if there were any php, it could be dropped in at any time!
+$text = get_option( 'wp_modal-referrer' );
+if ( ! $text ) {
+	$text = maybe_serialize( array (
+		'title' => 'Hello, fellow WordPresser',
+		'body' => '<p>It seems you've come here from a link on WordPress.org.<br>
+		If you're following up on a support question that we were discussing in a forum, please note:</p>
+		<p><em>What happens in the forums stays in the forums.</em></p>
+		<p><strong>Also be aware that bringing a forum argument here or to any other moderator’s site  is a violation of <a href="https://codex.wordpress.org/Forum_Welcome#The_Bad_Stuff" target=_blank> forum rules</a>.</strong>
+		</p>
+		<p>If, on the other hand, you're here to see who I am and what I'm up to, read on!</p>',
+		 )
+	 );
+	update_option( 'wp_modal_referrer', $text );
+}
+$content = maybe_unserialize( $text );
 ?>
 <div id="sdsModal" class="modal fade" role="dialog">
 <div class="modal-dialog">
 <!-- Modal content-->
 <div class="sds-modal-content">
 <div class="sds-modal-header">
-<h4 class="modal-title">Hello, fellow WordPresser!</h4>
+<h4 class="modal-title"><?php echo $content[ 'title' ]; ?></h4>
 </div>
 <div class="modal-body">
-<p>It seems you've come here from a link on WordPress.org.<br>
-If you're following up on a support question that we were discussing in a forum, please note:</p>
-<p><em>What happens in the forums stays in the forums.</em></p>
-<p><strong>Also be aware that bringing a forum argument here or to any other moderator’s site  is a violation of <a href="https://codex.wordpress.org/Forum_Welcome#The_Bad_Stuff" target=_blank> forum rules</a>.</strong>
-</p>
-<p>If, on the other hand, you're here to see who I am and what I'm up to, read on!</p>
+<?php echo $content[ 'body' ]; ?>
 </div>
 </div>
 
