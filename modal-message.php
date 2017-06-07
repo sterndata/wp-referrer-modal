794ecc7721132a37021b5359ed5f0e46cad921da
Sterndata <subscribed-lists@sterndata.com>
2017-06-07 12:25:32 -0500

    options page

--- modal-message.php
+++ modal-message.php
@@ -1,29 +1,28 @@
 <?php
-$text = get_option( 'wp_referrer-modal' );
-if ( ! $text ) {
-	$text = maybe_serialize( array (
-		'title' => 'Hello, fellow WordPresser',
-		'body' => '<p>It seems you came here from a link on WordPress.org.<br>
-		If you are following up on a support question that we were discussing in a forum, please note:</p>
-		<p><em>What happens in the forums stays in the forums.</em></p>
-		<p><strong>Also be aware that bringing a forum argument here or to any other moderator\’s site  is a violation of <a href="https://codex.wordpress.org/Forum_Welcome#The_Bad_Stuff" target=_blank> forum rules</a>.</strong>
-		</p>
-		<p>If, on the other hand, you are here to see who I am and what I am up to, read on!</p>',
-		 )
-	 );
-	update_option( 'wp_referrer-modal', $text );
+$title = get_option( 'sds_wp_referrer_modal_title' );
+$body = get_option( 'sds_wp_referrer_modal_body' );
+if ( ! $title ) {
+	$title = 'Hello, fellow WordPresser';
+	update_option( 'sds_wp_referrer_modal_title', $title );
 }
-$content = maybe_unserialize( $text );
+if ( ! $body ) {
+		$body = '
+		<p>It seems you came here from a link on WordPress.org.<br>If you are following up on a support question that we were discussing in a forum, please note:</p><p><em>What happens in the forums stays in the forums.</em></p><p><strong>Also be aware that bringing a forum argument here or to any other moderator site is a violation of <a href="https://codex.wordpress.org/Forum_Welcome#The_Bad_Stuff" target=_blank> forum rules</a>.</strong>
+		</p><p>If, on the other hand, you are here to see who I am and what I am up to, read on!</p>';
+
+		update_option( 'sds_wp_referrer_modal_body', $body );
+}
+
 ?>
 <div id="sdsModal" class="modal fade" role="dialog">
 <div class="modal-dialog">
 <!-- Modal content-->
 <div class="sds-modal-content">
 <div class="sds-modal-header">
-<h4 class="modal-title"><?php echo stripslashes( $content[ 'title' ] ); ?></h4>
+<h4 class="modal-title"><?php echo stripslashes( $title ); ?></h4>
 </div>
 <div class="modal-body">
-<?php echo stripslashes( $content[ 'body' ] ); ?>
+<?php echo stripslashes( $body ); ?>
 </div>
 </div>
 
--- sds-wp-modal.php
+++ sds-wp-modal.php
@@ -25,7 +25,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA	02110-1301, USA.
 *******************************/
 
 
-function sds_enqueue_scripts() {
+function sds_wp_referrer_modal_enqueue_scripts() {
 	$the_plugin = plugins_url( '', __FILE__ );
 	wp_enqueue_script( 'jquery-ui-core' );
 	wp_enqueue_script( 'jquery-ui-dialog ' );
@@ -33,9 +33,9 @@ function sds_enqueue_scripts() {
 	wp_enqueue_style( 'jquery-ui' , 'https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css' );
 	wp_enqueue_style( 'modal-referrer', $the_plugin . '/sds-wp-modal.css', array( 'jquery-ui' ) );
 }
-add_action( 'wp_enqueue_scripts', 'sds_enqueue_scripts' );
+add_action( 'wp_enqueue_scripts', 'sds_wp_referrer_modal_enqueue_scripts' );
 
-function sds_wp_modal_filter() {
+function sds_wp_referrer_modal_filter() {
 	ob_start();
 	?>
 <!-- Modal -->
@@ -43,14 +43,58 @@ function sds_wp_modal_filter() {
 <?php
 		echo ob_get_clean();
 }
-add_filter( 'wp_footer', 'sds_wp_modal_filter' );
+add_filter( 'wp_footer', 'sds_wp_referrer_modal_filter' );
 
 
 /*
  * autoptimize
 */
 
-add_filter('autoptimize_filter_css_exclude','sds_ao_override_cssexclude',10,1);
-function sds_ao_override_cssexclude($exclude) {
-	return $exclude.",sds-wp-modal.css";
+add_filter( 'autoptimize_filter_css_exclude','sds_wp_referrer_modal_ao_override_cssexclude',10,1 );
+function sds_wp_referrer_modal_ao_override_cssexclude( $exclude ) {
+	return $exclude . ',sds-wp-modal.css';
 }
+
+/*
+ *  Settings
+ */
+function sds_wp_referrer_modal_register_settings() {
+	add_option( 'sds_wp_referrer_modal_title', 'Title for the modal' );
+	add_option( 'sds_wp_referrer_modal_body',  'Body for the modal' );
+	register_setting( 'sds_wp_referrer_modal_options_group', 'sds_wp_referrer_modal_title', 'sds_wp_referrer_modal_sanitize' );
+	register_setting( 'sds_wp_referrer_modal_options_group', 'sds_wp_referrer_modal_body', 'sds_wp_referrer_modal_sanitize' );
+}
+ add_action( 'admin_init', 'sds_wp_referrer_modal_register_settings' );
+
+function sds_wp_referrer_modal_register_options_page() {
+	add_options_page( 'WP Referrer Modal Settings', 'WP Referrer Modal', 'manage_options', 'sds_wp_referrer_modal', 'sds_wp_referrer_modal_options_page' );
+}
+add_action( 'admin_menu', 'sds_wp_referrer_modal_register_options_page' );
+
+function sds_wp_referrer_modal_sanitize( $option ) {
+	$option = esc_html( $option );
+	return $option;
+}
+function sds_wp_referrer_modal_options_page() {
+?>
+  <div>
+	<?php screen_icon(); ?>
+  <h2>WP Referrer Modal</h2>
+  <form method="post" action="options.php">
+	<?php settings_fields( 'sds_wp_referrer_modal_options_group' ); ?>
+  <h3>Set Title and Body for the modal</h3>
+  <table>
+  <tr valign="top">
+  <th scope="row"><label for="sds_wp_referrer_modal_title">Title</label></th>
+  <td><input type="text" id="sds_wp_referrer-modal_title" name="sds_wp_referrer_modal_title" value="<?php echo get_option( 'sds_wp_referrer_modal_title' ); ?>" /></td>
+  </tr>
+	<tr valign="top">
+	<th scope="row"><label for="sds_wp_referrer-modal_body">Body</label></th>
+	<td><input type="text" id="sds_wp_referrer-modal_body" name="sds_wp_referrer_modal_body" value="<?php echo  get_option( 'sds_wp_referrer_modal_body' ); ?>" /></td>
+	</tr>
+  </table>
+	<?php  submit_button(); ?>
+  </form>
+  </div>
+<?php
+} ?>
