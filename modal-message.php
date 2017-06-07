db3cc31e414390f2574ca69d059b380f1f7238cb
Sterndata <subscribed-lists@sterndata.com>
2017-06-07 12:43:41 -0500

    remove extra tab before default msg

--- modal-message.php
+++ modal-message.php
@@ -6,8 +6,7 @@ if ( ! $title ) {
 	update_option( 'sds_wp_referrer_modal_title', $title );
 }
 if ( ! $body ) {
-		$body = '
-		<p>It seems you came here from a link on WordPress.org.<br>If you are following up on a support question that we were discussing in a forum, please note:</p><p><em>What happens in the forums stays in the forums.</em></p><p><strong>Also be aware that bringing a forum argument here or to any other moderator site is a violation of <a href="https://codex.wordpress.org/Forum_Welcome#The_Bad_Stuff" target=_blank> forum rules</a>.</strong>
+		$body = '<p>It seems you came here from a link on WordPress.org.<br>If you are following up on a support question that we were discussing in a forum, please note:</p><p><em>What happens in the forums stays in the forums.</em></p><p><strong>Also be aware that bringing a forum argument here or to any other moderator site is a violation of <a href="https://codex.wordpress.org/Forum_Welcome#The_Bad_Stuff" target=_blank> forum rules</a>.</strong>
 		</p><p>If, on the other hand, you are here to see who I am and what I am up to, read on!</p>';
 
 		update_option( 'sds_wp_referrer_modal_body', $body );
