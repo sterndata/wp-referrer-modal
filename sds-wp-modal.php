5ec12aa4b49f8f55091de94738fe518b5c210126
Sterndata <subscribed-lists@sterndata.com>
2017-06-07 23:18:54 -0500

    version number

--- sds-wp-modal.php
+++ sds-wp-modal.php
@@ -3,7 +3,7 @@
  * Plugin Name: WP Referrer Modal
  * Plugin URI:	https://github.com/sterndata/wp-referrer-modal
  * Description: warn about follow homes from wordpress.org
- * Version: 1.00
+ * Version: 1.0
  * Author: Stern Data Solutions
  * Author URI: http://www.sterndata.com
  * License: Gnu Public License V2
