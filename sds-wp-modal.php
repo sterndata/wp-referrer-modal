7844dae0528f8440e63a768f5cae7de0d1984f0c
Sterndata <subscribed-lists@sterndata.com>
2017-06-07 23:10:46 -0500

    merge the message into the main file

--- sds-wp-modal.php
+++ sds-wp-modal.php
@@ -3,7 +3,7 @@
  * Plugin Name: WP Referrer Modal
  * Plugin URI:	https://github.com/sterndata/wp-referrer-modal
  * Description: warn about follow homes from wordpress.org
- * Version: 0.99
+ * Version: 1.00
  * Author: Stern Data Solutions
  * Author URI: http://www.sterndata.com
  * License: Gnu Public License V2
@@ -24,6 +24,9 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA	02110-1301, USA.
 *******************************/
 
+/*
+* output all the necessary scripts and css
+*/
 
 function sds_wp_referrer_modal_enqueue_scripts() {
 	$the_plugin = plugins_url( '', __FILE__ );
@@ -35,16 +38,47 @@ function sds_wp_referrer_modal_enqueue_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'sds_wp_referrer_modal_enqueue_scripts' );
 
+/*
+ * display the messsages
+ *
+ * if there' not already set, update the options to use the default modal-message
+ */
+
 function sds_wp_referrer_modal_filter() {
 	ob_start();
-	?>
-<!-- Modal -->
-<?php include_once( plugin_dir_path( __FILE__ ) . '/modal-message.php' ); ?>
+	$title = get_option( 'sds_wp_referrer_modal_title' );
+	$body = get_option( 'sds_wp_referrer_modal_body' );
+	if ( ! $title ) {
+		$title = 'Hello, fellow WordPresser';
+		update_option( 'sds_wp_referrer_modal_title', $title );
+	}
+	if ( ! $body ) {
+		$body = '<p>It seems you came here from a link on WordPress.org.<br>If you are following up on a support question that we were discussing in a forum, please note:</p><p><em>What happens in the forums stays in the forums.</em></p><p><strong>Also be aware that bringing a forum argument here or to any other moderator site is a violation of <a href="https://codex.wordpress.org/Forum_Welcome#The_Bad_Stuff" target=_blank> forum rules</a>.</strong>
+		</p><p>If, on the other hand, you are here to see who I am and what I am up to, read on!</p>';
+
+		update_option( 'sds_wp_referrer_modal_body', $body );
+	}
+
+?>
+<div id="sdsModal" class="modal fade" role="dialog">
+<div class="modal-dialog">
+<!-- Modal content-->
+<div class="sds-modal-content">
+<div class="sds-modal-header">
+<h4 class="modal-title"><?php echo stripslashes( $title ); ?></h4>
+</div>
+<div class="modal-body">
+<?php echo stripslashes( $body ); ?>
+</div>
+</div>
+
+</div>
+</div>
+
 <?php
 		echo ob_get_clean();
 }
 add_filter( 'wp_footer', 'sds_wp_referrer_modal_filter' );
-
 
 /*
  * autoptimize
@@ -56,8 +90,9 @@ function sds_wp_referrer_modal_ao_override_cssexclude( $exclude ) {
 }
 
 /*
- *  Settings
+ *  Settings Dialog
  */
+
 function sds_wp_referrer_modal_register_settings() {
 	add_option( 'sds_wp_referrer_modal_title', 'Title for the modal' );
 	add_option( 'sds_wp_referrer_modal_body',  'Body for the modal' );
@@ -103,9 +138,9 @@ function sds_wp_referrer_modal_sanitize( $option ) {
 }
 
 function sds_wp_referrer_modal_add_settings_link( $links ) {
-    $settings_link = '<a href="options-general.php?page=sds_wp_referrer_modal">' . __( 'Settings' ) . '</a>';
-    array_push( $links, $settings_link );
-  	return $links;
+	$settings_link = '<a href="options-general.php?page=sds_wp_referrer_modal">' . __( 'Settings' ) . '</a>';
+	array_push( $links, $settings_link );
+	  return $links;
 }
 $plugin = plugin_basename( __FILE__ );
 add_filter( "plugin_action_links_$plugin", 'sds_wp_referrer_modal_add_settings_link' );
@@ -132,4 +167,4 @@ function sds_wp_referrer_modal_options_page() {
   </form>
   </div>
 <?php
-} ?>
+}
