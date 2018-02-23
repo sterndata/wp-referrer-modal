e7a40158f87a5a7b1702371bc4c1864fda973465
Xenos Konstantinos <mr.xkon@gmail.com>
2018-02-24 00:30:16 +0200

    refactor for awesome steve
    renaming class

--- sds-wp-modal.php
+++ sds-wp-modal.php
@@ -25,7 +25,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA	02110-1301, USA.
 *******************************/
 
-class SDS_WP_Referrer_Modal {
+class WP_Referrer_Modal {
 
 	/**
 	 * WP_Referrer_Modal constructor.
